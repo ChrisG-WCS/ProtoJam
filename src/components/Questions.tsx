8c6d57db40a970c59aedde1d62690608ae0c9151
DAMIEN FONTAINE <zelia.kowalski@gmail.com>
2025-05-21 20:27:07 +0200

    Add questions and options

---
+++ src/components/Questions.tsx
@@ -0,0 +1,123 @@
+export const Questions: Questions[] = [
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hyperlinks and Text Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "High Text Machine Language"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which tag is used to insert an image in an HTML page?",
+        options: [
+            "<img>",
+            "<image>",
+            "<src>",
+            "<picture>"
+        ],
+        correct: 1,
+    },
+    {
+        question: "How do you write a comment in HTML?",
+        options: [
+            "// This is a comment",
+            "/* This is a comment */",
+            "<!-- This is a comment -->",
+            "# This is a comment"
+        ],
+        correct: 1,
+    },
+    {
+        question: "What does the CSS property 'color' do?",
+        options: [
+            "Change the font",
+            "Change the text color",
+            "Change the text size",
+            "Change the background"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which tag is used to create a hyperlink?",
+        options: [
+            "<a>",
+            "<link>",
+            "<href>",
+            "<hyperlink>"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which CSS selector targets all <p> elements inside a div?",
+        options: [
+            "div.p",
+            "div > p",
+            "div + p",
+            "div p"
+        ],
+        correct: 1,
+    },
+    {
+        question: "How do you make text bold in CSS?",
+        options: [
+            "font-weight: bold;",
+            "text-style: bold;",
+            "font: bold;",
+            "style: bold;"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which HTML attribute makes a link open in a new tab?",
+        options: [
+            'new="tab"',
+            'target="_blank"',
+            'open="new"',
+            'window="blank"'
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which CSS property turns an element into a flexbox container?",
+        options: [
+            "flex: container;",
+            "display: flex;",
+            "flexbox: true;",
+            "container: flex;"
+        ],
+        correct: 1,
+    },
+    {
+        question: "How do you center an element horizontally using Flexbox?",
+        options: [
+            "justify-content: center;",
+            "align-items: center;",
+            "flex-align: center;",
+            "text-align: center;"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which of these HTML tags is semantic?",
+        options: [
+            "<span>",
+            "<div>",
+            "<section>",
+            "<font>"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which CSS unit adapts an element's size to the width of the window?",
+        options: [
+            "px",
+            "em",
+            "vw",
+            "pt"
+        ],
+        correct: 1,
+    },
+];
+
