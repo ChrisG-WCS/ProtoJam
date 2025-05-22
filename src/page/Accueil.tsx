251083c64b8c5b81f941192a592f70c787e7dd74
Jerome Marbach <jeromemarbach@outlook.fr>
2025-05-22 10:33:34 +0200

    I create the homepage

---
+++ src/page/Accueil.tsx
@@ -0,0 +1,11 @@
+import styles from "./Accueil.module.css";
+
+export const Accueil = () => {
+  return (
+    <>
+      <section>
+        <h1 className={styles.firstTitle}>WELCOME</h1>;
+      </section>
+    </>
+  );
+};
