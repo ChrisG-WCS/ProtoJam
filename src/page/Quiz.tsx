34555bedc725c7074acc2cb4537570160406c653
ChrisG-WCS <godet.chris59@gmail.com>
2025-05-22 23:06:34 +0200

    useContext

--- src/page/Quiz.tsx
+++ src/page/Quiz.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import questions from "../data/Questions";
 import Card from "../components/Card";
 import LivesDisplay from "../components/LivesDisplay";
+import { LivesProvider } from "../context/LivesContext";
 
 const Quiz = () => {
   const { id } = useParams();
@@ -9,16 +10,18 @@ const Quiz = () => {
   const question = questions.find((q) => q.id === questionId);
 
   return (
-    <section style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>ICI LE QUIZ</h1>
-      <p>Prépare-toi à tester tes connaissances !</p>
-      <LivesDisplay />
-      {question ? (
-        <Card key={question.id} question={question} />
-      ) : (
-        <p>Question introuvable</p>
-      )}
-    </section>
+    <LivesProvider>
+      <section style={{ padding: "2rem", textAlign: "center" }}>
+        <h1>ICI LE QUIZ</h1>
+        <p>Prépare-toi à tester tes connaissances !</p>
+        <LivesDisplay />
+        {question ? (
+          <Card key={question.id} question={question} />
+        ) : (
+          <p>Question introuvable</p>
+        )}
+      </section>
+    </LivesProvider>
   );
 };
 
