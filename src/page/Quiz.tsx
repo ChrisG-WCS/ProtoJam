4cc63c8ecaf401a102cc3671156bf79f3787eaa7
ChrisG-WCS <godet.chris59@gmail.com>
2025-05-22 14:46:14 +0200

    Add useParasm

--- src/page/Quiz.tsx
+++ src/page/Quiz.tsx
@@ -1,14 +1,17 @@
 import LivesDisplay from "../components/LivesDisplay";
+import { useParams } from "react-router-dom";
 
 const Quiz = () => {
+  const { id } = useParams();
+
   return (
-    <>
-      <section style={{ padding: "2rem", textAlign: "center" }}>
-        <h1>ICI LE QUIZ</h1>
-        <p>Prépare-toi à tester tes connaissances !</p>
-        <LivesDisplay />
-      </section>
-    </>
+    <section style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>ICI LE QUIZ</h1>
+      <p>Prépare-toi à tester tes connaissances !</p>
+      <p>Question numéro {id}</p>
+      <LivesDisplay />
+    </section>
   );
 };
+
 export default Quiz;
