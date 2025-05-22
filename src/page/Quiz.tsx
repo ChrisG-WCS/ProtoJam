37dee5fa94dcf93bafd2d8bca248a637da3066c4
ChrisG-WCS <godet.chris59@gmail.com>
2025-05-22 14:29:35 +0200

    Import LivesDisplay component in Quiz page

--- src/page/Quiz.tsx
+++ src/page/Quiz.tsx
@@ -1,9 +1,12 @@
+import LivesDisplay from "../components/LivesDisplay";
+
 const Quiz = () => {
   return (
     <>
       <section style={{ padding: "2rem", textAlign: "center" }}>
         <h1>ICI LE QUIZ</h1>
         <p>Prépare-toi à tester tes connaissances !</p>
+        <LivesDisplay />
       </section>
     </>
   );
