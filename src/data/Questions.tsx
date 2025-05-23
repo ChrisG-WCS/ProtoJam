215c30036dc589d49a9b30822ac608c9257d4cdd
ChrisG-WCS <godet.chris59@gmail.com>
2025-05-23 05:04:03 +0200

    modif data

--- src/data/Questions.tsx
+++ src/data/Questions.tsx
@@ -2,27 +2,27 @@ const questions = [
   {
     id: 1,
     text: "Quel élément HTML est utilisé pour insérer un titre principal sur une page ?",
-    options: ["<h1>", "<title>", "<header>", "<div>"],
+    options: ["<header>", "<h1>", "<title>", "<div>"],
     answer: "<h1>",
   },
   {
     id: 2,
     text: "Quelle propriété CSS permet de changer la couleur du texte ?",
-    options: ["color", "background-color", "font-size", "text-align"],
+    options: ["text-align", "font-size", "color", "background-color"],
     answer: "color",
   },
   {
     id: 3,
     text: "Quel attribut HTML est utilisé pour définir un lien hypertexte ?",
-    options: ["href", "src", "alt", "link"],
+    options: ["alt", "href", "src", "link"],
     answer: "href",
   },
   {
     id: 4,
     text: "Comment écrit-on un commentaire en CSS ?",
     options: [
-      "/* commentaire */",
       "// commentaire",
+      "/* commentaire */",
       "<!-- commentaire -->",
       "# commentaire",
     ],
@@ -31,39 +31,38 @@ const questions = [
   {
     id: 5,
     text: "Quel élément HTML est utilisé pour créer une liste non ordonnée ?",
-    options: ["<ul>", "<ol>", "<li>", "<list>"],
+    options: ["<list>", "<ul>", "<li>", "<ol>"],
     answer: "<ul>",
   },
   {
     id: 6,
     text: "Quelle propriété CSS modifie la taille de la police ?",
-    options: ["font-size", "text-size", "font-style", "size"],
+    options: ["text-size", "font-style", "font-size", "size"],
     answer: "font-size",
   },
   {
     id: 7,
     text: "Quel élément HTML permet d’insérer une image ?",
-    options: ["<img>", "<picture>", "<image>", "<src>"],
+    options: ["<image>", "<picture>", "<img>", "<src>"],
     answer: "<img>",
   },
   {
     id: 8,
     text: "Comment sélectionner tous les éléments `<p>` en CSS ?",
-    options: ["p", ".p", "#p", "*p"],
+    options: [".p", "*p", "p", "#p"],
     answer: "p",
   },
   {
     id: 9,
     text: "Quelle propriété CSS permet d’aligner le texte au centre ?",
-    options: ["text-align", "align-text", "center-text", "text-center"],
+    options: ["align-text", "text-center", "text-align", "center-text"],
     answer: "text-align",
   },
   {
     id: 10,
     text: "Quel attribut HTML est obligatoire pour l’élément `<img>` ?",
-    options: ["alt", "src", "title", "href"],
+    options: ["title", "href", "src", "alt"],
     answer: "src",
   },
 ];
-
 export default questions;
