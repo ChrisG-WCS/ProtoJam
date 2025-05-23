ad187a4ed43e34f6194a1dd4b86979f189a99543
ChrisG-WCS <godet.chris59@gmail.com>
2025-05-23 04:18:10 +0200

    Add Sega GIF and play Sega sound on click of the GIF

--- src/page/Accueil.tsx
+++ src/page/Accueil.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Accueil.module.css";
+import sega from "../images/sega.gif";
+import segaSound from "../assets/sounds/Sg.mp3";
 
 const Accueil = () => {
+  const [audio] = useState(new Audio(segaSound));
+
+  const playSound = () => {
+    audio.play();
+  };
+
   return (
-    <section>
-      <h1 className={styles.firstTitle}>WELCOME</h1>
-      <Link to="/quiz/1" className={styles.buttonStart}>
+    <section style={{ textAlign: "center" }}>
+      <img
+        src={sega}
+        alt="Welcome animation"
+        className={styles.welcomeGif}
+        style={{ width: "300px", height: "auto", cursor: "pointer" }}
+        onClick={playSound}
+      />
+      <Link
+        to="/quiz/1"
+        className={styles.buttonStart}
+        style={{ display: "block", marginTop: "20px" }}
+      >
         start
       </Link>
     </section>
